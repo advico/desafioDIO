3ca809666d69c9bd38f55921b4de021660e00b64
advico <adriawndev@gmail.com>
2024-07-21 00:54:01 -0300

    projeto entregue

---
+++ teste.rb
@@ -0,0 +1,14 @@
+
+print "Digite seu nome: "
+nome = gets.chomp
+
+
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+
+
+print "Digite sua idade: "
+idade = gets.chomp
+
+
+puts "Olá, #{nome} #{sobrenome}. Você tem #{idade} anos."
